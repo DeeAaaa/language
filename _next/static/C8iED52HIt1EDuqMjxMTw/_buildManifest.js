74d64cc22205984569787d34bf7ff4baf3f49716
DeeAaaa <Deeaco@outlook.com>
2026-07-18 18:40:54 +0800

    Updates

--- _next/static/C8iED52HIt1EDuqMjxMTw/_buildManifest.js
+++ _next/static/C8iED52HIt1EDuqMjxMTw/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/language//_next/:path+",
+        "destination": "/language/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
